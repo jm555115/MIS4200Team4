5ad014131d31c386a814eef89dd583492794bf04
Musselman <jm555115@ohio.edu>
2019-10-07 10:08:00 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200Team4.Startup))]
+namespace MIS4200Team4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
