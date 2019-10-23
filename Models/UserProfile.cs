2ceac65170a0652da1f2ed4b0f7746dfb38af1ef
Megan Schwetschenau <ms837613@ohio.edu>
2019-10-23 10:30:13 -0400

    added models

---
+++ Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team4.Models
+{
+    public class UserProfile
+    {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ Models/userDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team4.Models
+{
+    public class userDetail
+    {
+        public int userDetailID { get; set; }
+        //public bool stewardship { get; set; }
+        //public bool culture { get; set; }
+
+        //public bool deliveryExcellence { get; set; }
+        //public bool innovation { get; set; }
+        //public bool greaterGood { get; set; }
+        //public bool integrityAndOpenness { get; set; }
+        //public bool balance { get; set; }
+    }
+}
