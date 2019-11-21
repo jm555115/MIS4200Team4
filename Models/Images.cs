10aaa272106df80c8f43f06d5bdae700bd67683d
Jersey Musselman <jm555115@ohio.edu>
2019-11-20 19:20:15 -0500

    Image controller, & other stuff

---
+++ Models/Images.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team4.Models
+{
+    public class Images
+    {
+        public Guid imageId { get; set; }
+        public string imageName { get; set; }
+        public string uploadedImages { get; set; }
+        public string imageComments { get; set; }
+
+    }
+}
